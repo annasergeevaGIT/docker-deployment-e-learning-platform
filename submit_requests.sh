c8149383b95d0268aa3028c84c0844252f84577d
Sergeeva Anna <sirgeeva1@gmail.com>
2025-11-14 23:05:54 +0100

    fix: security config

--- submit_requests.sh
+++ submit_requests.sh
@@ -1,53 +1,79 @@
 #!/bin/bash
-echo -e "\n*** Submitting requests to Course Service ***\n"
 
+CLIENT_SECRET=RleFn4MVPDKtGTXIZv4Opyfuwfx2fFLL
+
+echo -e "\n*** Submitting requests to Course Service***\n"
+
+echo -e "\n*** Get access_token for alex***\n"
+ACCESS_TOKEN=$(curl -X POST http://keycloak:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=alex' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d "client_secret=$CLIENT_SECRET" -d 'scope=openid roles' -s | jq .access_token -r)
+AUTH_HEADER="Authorization: Bearer $ACCESS_TOKEN"
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/courses -H 'Content-Type: application/json' -d '{"name": "One","description": "Nice Item One", "price": 10.10, "category": "engineering", "duration": 1000, "difficulty": "beginner", "imageUrl": "http://images.com/one.png", "moduleCollection": {"modules": [{"name": "module one", "duration": 10}, {"name": "module two", "duration": 20}]}}'
+curl -X POST http://localhost:9099/v1/courses -H "$AUTH_HEADER" -H 'Content-Type: application/json' -d '{"name": "One","description": "Nice Item One", "price": 10.10, "category": "engineering", "duration": 1000, "difficulty": "beginner", "imageUrl": "http://images.com/one.png", "moduleCollection": {"modules": [{"name": "module one", "duration": 10}, {"name": "module two", "duration": 20}]}}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/courses -H 'Content-Type: application/json' -d '{"name": "Two","description": "Nice Item Two", "price": 10.10, "category": "engineering", "duration": 1000, "difficulty": "beginner", "imageUrl": "http://images.com/one.png", "moduleCollection": {"modules": [{"name": "module one", "duration": 10}, {"name": "module two", "duration": 20}]}}'
+curl -X POST http://localhost:9099/v1/courses -H "$AUTH_HEADER" -H 'Content-Type: application/json' -d '{"name": "Two","description": "Nice Item Two", "price": 10.10, "category": "engineering", "duration": 1000, "difficulty": "beginner", "imageUrl": "http://images.com/one.png", "moduleCollection": {"modules": [{"name": "module one", "duration": 10}, {"name": "module two", "duration": 20}]}}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/courses -H 'Content-Type: application/json' -d '{"name": "Three","description": "Nice Item Three", "price": 10.10, "category": "engineering", "duration": 1000, "difficulty": "beginner", "imageUrl": "http://images.com/one.png", "moduleCollection": {"modules": [{"name": "module one", "duration": 10}, {"name": "module two", "duration": 20}]}}'
+curl -X POST http://localhost:9099/v1/courses -H "$AUTH_HEADER" -H 'Content-Type: application/json' -d '{"name": "Three","description": "Nice Item Three", "price": 10.10, "category": "engineering", "duration": 1000, "difficulty": "beginner", "imageUrl": "http://images.com/one.png", "moduleCollection": {"modules": [{"name": "module one", "duration": 10}, {"name": "module two", "duration": 20}]}}'
 printf "\n\n"
+
 echo -e "\n*** Submitting requests to Enrollments Service ***\n"
+
+echo -e "\n*** Get access_token for jane***\n"
+ACCESS_TOKEN=$(curl -X POST http://keycloak:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=jane' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d "client_secret=$CLIENT_SECRET" -d 'scope=openid roles' -s | jq .access_token -r)
+AUTH_HEADER="Authorization: Bearer $ACCESS_TOKEN"
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H 'X-User-Name: UserName1' -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Moscow", "street": "Street", "house": 1, "apartment": 1}}'
+curl -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Moscow", "street": "Street", "house": 1, "apartment": 1}}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H 'X-User-Name: UserName2' -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Kaluga", "street": "Street", "house": 1, "apartment": 1}}'
+curl -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Graz", "street": "Street", "house": 1, "apartment": 1}}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H 'X-User-Name: UserName3' -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Samara", "street": "Street", "house": 1, "apartment": 1}}'
+curl -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Berlin", "street": "Street", "house": 1, "apartment": 1}}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H 'X-User-Name: UserName4' -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Rostov", "street": "Street", "house": 1, "apartment": 1}}'
+
+echo -e "\n*** Get access_token for max***\n"
+ACCESS_TOKEN=$(curl -X POST http://keycloak:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=max' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d "client_secret=$CLIENT_SECRET" -d 'scope=openid roles' -s | jq .access_token -r)
+AUTH_HEADER="Authorization: Bearer $ACCESS_TOKEN"
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H 'X-User-Name: UserName5' -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Rostov", "street": "Street", "house": 1, "apartment": 1}}'
+curl -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Rostov", "street": "Street", "house": 1, "apartment": 1}}'
 printf "\n\n"
+curl -X POST http://localhost:9099/v1/course-enrollments -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseNames": ["One", "Two", "Three"], "address": { "city": "Rostov", "street": "Street", "house": 1, "apartment": 1}}'
+printf "\n\n"
+
 echo -e "\n*** Submitting requests to Feedback Service ***\n"
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: Alex' -d '{"courseId": 1, "comment": "Comment", "rate": 5}'
+curl -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseId": 1, "comment": "Comment", "rate": 5}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: Alex' -d '{"courseId": 2, "comment": "Comment", "rate": 5}'
+curl -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseId": 2, "comment": "Comment", "rate": 5}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: Alex' -d '{"courseId": 3, "comment": "Comment", "rate": 5}'
+curl -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseId": 3, "comment": "Comment", "rate": 5}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: John' -d '{"courseId": 1, "comment": "Comment", "rate": 5}'
+
+echo -e "\n*** Get access_token for jane***\n"
+ACCESS_TOKEN=$(curl -X POST http://keycloak:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=jane' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d "client_secret=$CLIENT_SECRET" -d 'scope=openid roles' -s | jq .access_token -r)
+AUTH_HEADER="Authorization: Bearer $ACCESS_TOKEN"
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: Michael' -d '{"courseId": 1, "comment": "Comment", "rate": 3}'
+curl -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseId": 1, "comment": "Comment", "rate": 5}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: Max' -d '{"courseId": 1, "comment": "Comment", "rate": 2}'
+curl -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseId": 4, "comment": "Comment", "rate": 3}'
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H 'X-User-Name: Phill' -d '{"courseId": 1, "comment": "Comment", "rate": 2}'
+curl -X POST http://localhost:9099/v1/feedbacks -H 'Content-Type: application/json' -H "$AUTH_HEADER" -d '{"courseId": 5, "comment": "Comment", "rate": 2}'
 printf "\n\n"
 curl http://localhost:9099/v1/feedbacks/1
 printf "\n\n"
-curl -v -H 'X-User-Name: Alex' "http://localhost:9099/v1/feedbacks/course/1?from=0&size=10&sortBy=date_asc"
+curl "http://localhost:9099/v1/feedbacks/course/1?from=0&size=10&sortBy=date_asc"
 printf "\n\n"
-curl -v -H 'X-User-Name: Alex' "http://localhost:9099/v1/feedbacks/my?from=0&size=10&sortBy=date_asc"
+
+echo -e "\n*** Get access_token for max***\n"
+ACCESS_TOKEN=$(curl -X POST http://keycloak:8080/realms/cloud-java/protocol/openid-connect/token -d 'username=max' -d 'password=password' -d 'grant_type=password' -d 'client_id=cloud-java-gateway' -d "client_secret=$CLIENT_SECRET" -d 'scope=openid roles' -s | jq .access_token -r)
+AUTH_HEADER="Authorization: Bearer $ACCESS_TOKEN"
 printf "\n\n"
-curl -v -X POST http://localhost:9099/v1/feedbacks/ratings -H 'Content-Type: application/json' -d '{"courseIds": [1,2,3,4,5]}'
+curl -H "$AUTH_HEADER" "http://localhost:9099/v1/feedbacks/my?from=0&size=10&sortBy=date_asc"
 printf "\n\n"
+curl -X POST http://localhost:9099/v1/feedbacks/ratings -H 'Content-Type: application/json' -d '{"courseIds": [1,2,3,4,5]}'
+printf "\n\n"
+
 echo -e "\n*** Submitting requests to Course Aggregate Service ***\n"
 printf "\n\n"
-curl -v http://localhost:9099/v1/course-aggregate/1
+curl http://localhost:9099/v1/course-aggregate/1
 printf "\n\n"
-curl -v "http://localhost:9099/v1/course-aggregate?category=ENGINEERING"
+curl "http://localhost:9099/v1/course-aggregate?category=ENGINEERING"
 printf "\n\n"
 
